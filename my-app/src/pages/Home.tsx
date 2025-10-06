6a0c399dad8e9c09d46417f66ac9f63feeae091f
mehemmedhzr <mehemmed.hezerxanli@gmail.com>
2025-10-06 18:41:29 +0400

    test commit

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold">Welcome</h1>
+        <h1 className="text-4xl font-bold">Welcomed</h1>
       </div>
     );
   }
